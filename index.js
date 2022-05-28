795c6e085fabff13fbdb783dcd2c440e14b985a6
jmcraineydev <jmcraineydev@gmail.com>
2022-05-28 17:54:00 +0900

    Build: add inital build and layout

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 (function () {
-  let h1= document.getElementById('title')
-  h1.innerText = "Hello World"
-})()
+  let h1 = document.getElementById("title");
+  h1.innerText = "New Account";
+})();
+
+const password = document.getElementById("password");
+
+password.oninput = isValidPassword(Event);
+
+function isValidPassword(event) {
+  const pwPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+  console.log(event.value);
+  //console.log(event);
+}
